8b8dc1719ba5466df6edcde488e0ae7da54e75f8
Olga Pavlova <olgapavlova@gmail.com>
2023-05-19 12:49:05 +0300

    Minimum Size Subarray Sum

--- Array and String/Minimum Size Subarray Sum.py
+++ Array and String/Minimum Size Subarray Sum.py
@@ -1,33 +1,65 @@
-nums = [2,3,1,2,4,3]
-target = 7
+nums = [10,2,3]
+target = 6
 
 i2 = 0
-s = nums[i2]
-minlen = len(nums)
+current = 0
+dir = 1
+minsum = []
 
 for i1 in range(len(nums)):
-    if i1 > 0: s -= nums[i1 - 1]
+    result = 'looking'
 
-    if i1 == i2 + 1:
-        s += nums[i2]
-        i2 = i1
-
-    print(i1, i2, '--', s)
-
-    if (s < target):
-        while (s < target) and (i2 < len(nums) - 1):
-            i2 += 1
-            s += nums[i2]
-            print(i1, i2, '-', s)
+    if (i1 <= i2):
+        if (i1 > 0): current -= nums[i1 - 1] # был сдвиг — надо вычесть
+        else: current = nums[0] # первый пункт — сдвига не было
     else:
-        while (s > target) and (i2 >= i1):
-            s -= nums[i2]
-            print(i1, i2, '-', s)
-            i2 -= 1
-        i2 += 1
-        s += nums[i2]
+        if (i2 < len(nums) - 1):
+            i2 += 1 # оба указателя только что указывали на одну точку — должны продолжать
+            current = nums[i2] # сумма — это значение в точке 
+        else: result = 'fail'
 
-    if (s >= target): minlen = min(minlen, i2 - i1 + 1)
-    print(minlen)
+    if current == target:
+        result = 'found' # TODO Check necessity
+        minsum.append(i2 - i1 + 1)
 
-print(i1, i2, s)
+    if (current > target) and (i2 == i1):
+        result = 'found' # TODO Check necessity
+        minsum.append(1)
+
+    if current < target:
+        # print('Вверх')
+        dir = 1
+    if current > target:
+        # print('Вниз')
+        dir = -1
+
+    print(f"= i1 = {i1}, i2 = {i2}, current = {current}, result = {result}, dir = {dir}")
+
+    while result == 'looking':
+        i2 += dir
+        # print(f"— — i2 = {i2}, dir = {dir}")
+
+        if (i1 <= i2) and (i2 < len(nums)):
+            if dir == 1: current += nums[i2]
+            elif i2 < len(nums) - 1: current -= nums[i2 + 1]
+
+            if (dir == -1) and (current < target):
+                result = 'found'
+                minsum.append(i2 - i1 + 2)
+            
+            if (dir == 1) and (current > target):
+                result = 'found'
+                minsum.append(i2 - i1 + 1)
+            
+            if (current == target):
+                result = 'found'
+                minsum.append(i2 - i1 + 1)
+
+            print(f"• i1 = {i1}, i2 = {i2}, current = {current}, result = {result}")
+
+        else:
+            result = 'fail'
+            i2 -= dir
+
+print(minsum)
+
