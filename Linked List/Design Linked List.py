01ff0c175214e2a01d1a324d3fcedb4d5d08a9ab
olgapavlova <olgapavlova@gmail.com>
2023-06-06 22:26:56 +0300

    Design Linked List (except deleteAtIndex)

--- Linked List/Design Linked List.py
+++ Linked List/Design Linked List.py
@@ -1,100 +1,114 @@
-class MyLinkedList:
+class Node():
+    """ Элемент связного списка """
+    def __init__(self, val):
+        self.val = val
+        self.next = None
 
+class MyLinkedList:
+    """ Связный список """
     def __init__(self):
-        pass
+        self.head = None
 
     def get(self, index: int) -> int:
-        pointer = self
-        result = -1
-        for i in range(index + 1):
+        pointer = self.head
+        allowGet = hasattr(self.head, 'val')
+        for i in range(index):
             if hasattr(pointer, 'next'):
-                result = pointer.val
                 pointer = pointer.next
-            else: result = -1
-        return result
+            else: allowGet = False
+        if allowGet:
+            if hasattr(pointer, 'val'):
+                return pointer.val
+            else: return -1
+        else:
+            return -1
         
     def addAtHead(self, val: int) -> None:
-        pointer = self
-        next_val = val
-        while hasattr(pointer, 'next'):
-            next_val, pointer.val = pointer.val, next_val        
-            pointer = pointer.next
-        pointer.next = MyLinkedList()
-        pointer.val = next_val
+        first = self.head
+        self.head = Node(val)
+        self.head.next = first
 
     def addAtTail(self, val: int) -> None:
-        pointer = self
-        while hasattr(pointer, 'next'):
-            # print(pointer.next)
-            pointer = pointer.next
-        pointer.val = val
-        pointer.next = MyLinkedList()
+        pointer = self.head
+        if self.head is None:
+            self.head = Node(val)
+        else:
+            while hasattr(pointer, 'next') and pointer.next:
+                pointer = pointer.next
+            pointer.next = Node(val)
 
     def addAtIndex(self, index: int, val: int) -> None:
-        pointer = self
-        ins = MyLinkedList()
-        ins.val = val
-        invalid = False
-        for i in range(index-1):
-            if hasattr(pointer, 'next'):
-                pointer = pointer.next
-            else: invalid = True
-        if invalid == False:
-            ins.next = pointer.next
-            pointer.next = ins        
+        pointer = self.head
+        if index == 0:
+            self.addAtHead(val)
+        else:
+            position = 0
+            allowInsert = True
+            for i in range(1, index):
+                if hasattr(pointer, 'next') and pointer.next:
+                    # print(position, pointer.val)
+                    pointer = pointer.next
+                    position += 1
+                else: allowInsert = False
+            # print(position, ' ! ', index)
+            if (position < index - 2): allowInsert = False
+            if allowInsert:
+                if pointer:
+                    insNode = Node(val)
+                    insNode.next = pointer.next
+                    pointer.next = insNode
+                else:
+                    self.addAtTail(val)
 
     def deleteAtIndex(self, index: int) -> None:
-        pointer = self
-        for i in range(index-1):
-            if hasattr(pointer, 'next'):
-                pointer = pointer.next
-        
-        if index > 0 and hasattr(pointer, 'next') and hasattr(pointer.next, 'next'):
-            # print('$$$', pointer.next.val)
-            pointer.next = pointer.next.next
-            # print('%%%', pointer.next.val)
-            # print('123')
-        elif index == 1 and hasattr(pointer, 'next'):
-            self.next = MyLinkedList()
-            # print('456')
-        elif index == 0 and hasattr(pointer, 'next'):
-            pointer = self.next
-            print('===', id(pointer.next))
-            self = pointer
-            print('===', id(self))
-            print('789')
-            print(self.val)
-            print('===', id(self))
-            print('===', id(myLinkedList))
-        else:
-            del(self.val)
-            del(self.next)
-            print('012')
-
+        pass
 
 myLinkedList = MyLinkedList()
-
-myLinkedList.addAtHead(1)
+myLinkedList.addAtHead(7)
+myLinkedList.addAtTail(2)
+myLinkedList.addAtHead(5)
+myLinkedList.addAtTail(4)
+print([myLinkedList.get(i) for i in range(7)])
+myLinkedList.addAtIndex(0, 9)
+print([myLinkedList.get(i) for i in range(7)])
+myLinkedList.addAtIndex(3, 99)
+print([myLinkedList.get(i) for i in range(7)])
+myLinkedList.addAtIndex(7, 90)
+print([myLinkedList.get(i) for i in range(8)])
 """ print(myLinkedList.get(0))
+print(myLinkedList.get(1))
+print(myLinkedList.get(2))
+print(myLinkedList.get(3))
+print(myLinkedList.get(4)) """
+
+""" two = Node(5)
+myLinkedList.head.next = two
+print(myLinkedList.get(0))
+print(myLinkedList.get(2))
+ """
+
+
+""" myLinkedList.addAtHead(1)
+print(myLinkedList.get(0))
 print(myLinkedList.get(1)) """
 
-myLinkedList.addAtTail(3)
-""" print(myLinkedList.get(0))
+""" myLinkedList.addAtTail(3)
+print(myLinkedList.get(0))
 print(myLinkedList.get(1))
 print(myLinkedList.get(2)) """
 
-myLinkedList.addAtIndex(1, 2)    # linked list becomes 1->2->3
+""" myLinkedList.addAtIndex(1, 2)    # linked list becomes 1->2->3
 myLinkedList.get(1)              # return 2
-""" print(myLinkedList.get(0))
+print(myLinkedList.get(0))
 print(myLinkedList.get(1))
 print(myLinkedList.get(2)) """
 
 
-myLinkedList.deleteAtIndex(0)    # now the linked list is 1->3
+""" myLinkedList.deleteAtIndex(0)    # now the linked list is 1->3
 myLinkedList.get(1)              # return 3
 print(myLinkedList.get(0))
 print('===', id(myLinkedList))
-
+ """
 # print(myLinkedList.val)
 
 
